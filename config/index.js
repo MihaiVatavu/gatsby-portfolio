a3f439aac3c8ce224979c2ff9ab33f153a828276
MihaiVatavu <66440300+MihaiVatavu@users.noreply.github.com>
2021-03-23 15:25:49 +0000

    Updated metadata

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   //-- SITE SETTINGS -----
   siteTitle: "Gatsby Starter Portfolio Minimal",
+  siteMetaData:"Mihai Vatavu",
   siteShortTitle: "Mihai Vatavu", // Used as logo text in header, footer, and splash screen
   siteDescription:
     "A modern one-page portfolio with a clean yet expressive design.",
